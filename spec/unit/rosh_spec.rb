eabc62405e3367a0f5e16ae89d6bfcbe71270a00
Steve Loveless <steve.loveless@gmail.com>
2013-04-06 19:57:38 -0700

    Renamed host to current_host

--- spec/unit/rosh_spec.rb
+++ spec/unit/rosh_spec.rb
@@ -7,24 +7,24 @@ describe Rosh do
   end
 
   describe '#add_host' do
-    let(:host) do
+    let(:current_host) do
       double 'Rosh::Host'
     end
 
     before do
-      Rosh::Host.should_receive(:new).and_return(host)
+      Rosh::Host.should_receive(:new).and_return(current_host)
     end
 
     it 'creates a Host by the given hostname and adds it to the list of hosts' do
       subject.add_host('test')
-      subject.hosts.should == { 'test' => host }
+      subject.hosts.should == { 'test' => current_host }
     end
 
     context 'with an alias' do
       it 'lets you refer to the host by its alias' do
         subject.add_host('test', :thing)
 
-        subject.hosts.should == { thing: host }
+        subject.hosts.should == { thing: current_host }
       end
     end
   end
