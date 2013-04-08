a4bba2c8f78f0e85262dd932cddda13885d5ff97
Steve Loveless <steve.loveless@gmail.com>
2013-04-07 22:14:41 -0700

    Ported rc file loading

--- lib/rosh.rb
+++ lib/rosh.rb
@@ -1,11 +1,15 @@
+require 'yaml'
 require_relative 'rosh/host'
 
 
 class Rosh
   attr_reader :hosts
 
+  DEFAULT_RC_FILE = '.roshrc.yml'
+
   def initialize
     @hosts = {}
+    @config = nil
   end
 
   def add_host(hostname, host_alias=nil, **ssh_options)
@@ -13,6 +17,15 @@ class Rosh
       @hosts[hostname] = Rosh::Host.new(hostname, ssh_options)
     else
       @hosts[host_alias] = Rosh::Host.new(hostname, ssh_options)
+    end
+  end
+
+  def config
+    return @config if @config
+
+    if File.exists? DEFAULT_RC_FILE
+      erb = ERB.new(File.read(DEFAULT_RC_FILE))
+      @config = YAML.load(erb.result)
     end
   end
 end
