6778fb5e232dc17ef2a3aecfa4fcab3a4588660a
Steve Loveless <steve.loveless@gmail.com>
2013-04-22 22:59:13 -0700

    Added docs and specs

--- lib/rosh/host/remote_file_system_object.rb
+++ lib/rosh/host/remote_file_system_object.rb
@@ -6,7 +6,13 @@ class Rosh
     class RemoteFileSystemObject
       include Observable
 
-      def self.create(path, remote_shell)
+      # @param [String] path Path to the object.
+      # @param [Rosh::Host::Shells::Remote] remote_shell
+      # @param [Symbol] force_type Make the object as a certain type.  +dir+,
+      #   +file+, +link+.  Used when the object doesn't yet exist on the file
+      #   system.
+      # @return [Rosh::Host::RemoteDir,Rosh::Host::RemoteFile,Rosh::Host::RemoteLink]
+      def self.create(path, remote_shell, force_type: nil)
         fso = new(path, remote_shell)
 
         if fso.directory?
@@ -15,18 +21,30 @@ class Rosh
           Rosh::Host::RemoteFile.new(path, remote_shell)
         elsif fso.link?
           Rosh::Host::RemoteLink.new(path, remote_shell)
+        elsif force_type
+          klass = Rosh::Host.const_get("Remote#{force_type.capitalize}".to_sym)
+          klass.new(path, remote_shell)
         end
       end
 
+      attr_reader :path
+
+      # @param [String] path Path to the remote file system object.
+      # @param [Rosh::Host::Shells::Remote] remote_shell
       def initialize(path, remote_shell)
         @path = path
         @remote_shell = remote_shell
       end
 
+      # Returns the pathname used to create file as a String. Does not normalize
+      # the name.
+      #
+      # @return [String]
       def to_path
         @path
       end
 
+      # @return [Boolean] +true+ if the object is a file; +false+ if not.
       def file?
         cmd = "[ -f #{@path} ]"
         @remote_shell.exec(cmd)
@@ -34,6 +52,7 @@ class Rosh
         @remote_shell.last_exit_status.zero?
       end
 
+      # @return [Boolean] +true+ if the object is a directory; +false+ if not.
       def directory?
         cmd = "[ -d #{@path} ]"
         @remote_shell.exec(cmd)
@@ -41,6 +60,7 @@ class Rosh
         @remote_shell.last_exit_status.zero?
       end
 
+      # @return [Boolean] +true+ if the object is a link; +false+ if not.
       def link?
         cmd = "[ -L #{@path} ]"
         @remote_shell.exec(cmd)
@@ -48,6 +68,8 @@ class Rosh
         @remote_shell.last_exit_status.zero?
       end
 
+      # @return [Boolean] +true+ if the object exists on the file system;
+      #   +false+ if not.
       def exists?
         cmd = "[ -e #{@path} ]"
         @remote_shell.exec(cmd)
@@ -55,55 +77,61 @@ class Rosh
         @remote_shell.last_exit_status.zero?
       end
 
+      # @return [String] The owner of the file system object.
       def owner
         cmd = "ls -l #{@path} | awk '{print $3}'"
 
         @remote_shell.exec(cmd).strip
       end
 
+      # Sets the file system object to +new_owner+.  If the update was a
+      # success, the shell's #last_exit_status will be 0, which will be the case
+      # even if the owner was set to the same owner.  If the update changes the
+      # file system object, the observer's #update method gets called.
+      #
+      # @param [String] new_owner The user name to make the new owner of the file
+      #   system object.
       def owner=(new_owner)
         old_owner = owner
-        puts "old owner: #{old_owner}"
-        puts "new owner: #{new_owner}"
         cmd = "chown #{new_owner} #{@path}"
         @remote_shell.exec(cmd)
 
-        success = @remote_shell.last_exit_status.zero?
-
-        if success && old_owner != new_owner
+        if @remote_shell.last_exit_status.zero? && old_owner != new_owner
           changed
           notify_observers(self, attribute: :owner, old: old_owner, new: new_owner)
         end
-
-        success
       end
 
+      # @return [String] The group of the file system object.
       def group
         cmd = "ls -l #{@path} | awk '{print $4}'"
 
         @remote_shell.exec(cmd).strip
       end
 
+      # Sets the group on the file system object to +new_group+.  If the update was a
+      # success, the shell's #last_exit_status will be 0, which will be the case
+      # even if the group was set to the same group.  If the update changes the
+      # file system object, the observer's #update method gets called.
+      #
+      # @param [String] new_group The group name to make the new group of the file
+      #   system object.
       def group=(new_group)
         old_group = group
         cmd = "chgrp #{new_group} #{@path}"
         @remote_shell.exec(cmd)
 
-        success = @remote_shell.last_exit_status.zero?
-
-        if success && old_group != new_group
+        if @remote_shell.last_exit_status.zero? && old_group != new_group
           changed
           notify_observers(self, attribute: :group, old: old_group, new: new_group)
         end
-
-        success
       end
 
+      # @return [Integer] The mode of the file system object.
       def mode
         cmd = "ls -l #{@path} | awk '{print $1}'"
         letter_mode = @remote_shell.exec(cmd)
-        puts "letter mode: #{letter_mode}"
-        %r[^(?<type>.)(?<user>.{3})(?<group>.{3})(?<others>.{3})] =~ letter_mode
+        %r[^(?<type>.)(?<user>.{3})(?<group>.{3})(?<others>.{3})] =~ letter_mode.strip
 
         converter = lambda do |letters|
           value = 0
@@ -120,42 +148,57 @@ class Rosh
           value.to_s
         end
 
-        number_mode = ''
-        number_mode << converter.call(user)
-        number_mode << converter.call(group)
-        number_mode << converter.call(others)
+        if user && group && others
+          number_mode = ''
+          number_mode << converter.call(user)
+          number_mode << converter.call(group)
+          number_mode << converter.call(others)
 
-        number_mode.to_i
+          number_mode.to_i
+        else
+          nil
+        end
       end
 
+      # Sets the mode on the file system object to +new_mode+.  If the update was a
+      # success, the shell's #last_exit_status will be 0, which will be the case
+      # even if the mode was set to the same mode.  If the update changes the
+      # file system object, the observer's #update method gets called.
+      #
+      # @param [Integer] new_mode The mode to set on the file
+      #   system object.
       def mode=(new_mode)
         old_mode = mode
         cmd = "chmod #{new_mode} #{@path}"
         @remote_shell.exec(cmd)
 
-        success = @remote_shell.last_exit_status.zero?
-
-        if success && old_mode != new_mode.to_i
+        if @remote_shell.last_exit_status.zero? && old_mode != new_mode.to_i
           changed
           notify_observers(self, attribute: :mode, old: old_mode, new: new_mode)
         end
-
-        success
       end
 
+      # Just like Ruby's File#basename, returns the base name of the object.
+      #
+      # @return [String]
       def basename
         File.basename(@path)
       end
 
+      # Removes the file system object from the remote host.  If the removal was
+      # successful, the shell's #last_exit_status will be 0, which will be the
+      # case if the file didn't exist before making the call.  If the removal
+      # actually removes the object, the observer's #update method gets called.
       def remove
+        existed = exists?
         cmd = "rm -rf #{@path}"
         @remote_shell.exec(cmd)
 
         success = @remote_shell.last_exit_status.zero?
 
-        if success
+        if success && existed
           changed
-          notify_observers(self, attribute: :remove, old: @path, new: nil)
+          notify_observers(self, attribute: :path, old: @path, new: nil)
         end
 
         success
---
+++ spec/unit/rosh/host/remote_file_system_object_spec.rb
@@ -0,0 +1,436 @@
+require 'spec_helper'
+require 'rosh/host/remote_file'
+
+
+describe Rosh::Host::RemoteFileSystemObject do
+  subject do
+    Rosh::Host::RemoteFileSystemObject.new(path, shell)
+  end
+
+  let(:path) { '/file' }
+  let(:shell) { double 'Rosh::Host::Shells::Remote' }
+
+  describe '.create' do
+    before do
+      Rosh::Host::RemoteFileSystemObject.should_receive(:new).
+        and_return fso
+    end
+
+    context 'path is a directory' do
+      let(:fso) do
+        f = double 'Rosh::Host::RemoteDir'
+        f.should_receive(:directory?).and_return true
+
+        f
+      end
+
+      it 'returns a new Rosh::Host::RemoteDir' do
+        Rosh::Host::RemoteDir.should_receive(:new).with('dir', shell).
+          and_return 'the dir'
+        Rosh::Host::RemoteFileSystemObject.create('dir', shell).
+          should eq 'the dir'
+      end
+    end
+
+    context 'path is a file' do
+      let(:fso) do
+        f = double 'Rosh::Host::RemoteFile'
+        f.should_receive(:directory?).and_return false
+        f.should_receive(:file?).and_return true
+
+        f
+      end
+
+      it 'returns a new Rosh::Host::RemoteFile' do
+        Rosh::Host::RemoteFile.should_receive(:new).with('file', shell).
+          and_return 'the file'
+        Rosh::Host::RemoteFileSystemObject.create('file', shell).
+          should eq 'the file'
+      end
+    end
+
+    context 'path is a link' do
+      let(:fso) do
+        f = double 'Rosh::Host::RemoteLink'
+        f.should_receive(:directory?).and_return false
+        f.should_receive(:file?).and_return false
+        f.should_receive(:link?).and_return true
+
+        f
+      end
+
+      it 'returns a new Rosh::Host::RemoteLink' do
+        Rosh::Host::RemoteLink.should_receive(:new).with('link', shell).
+          and_return 'the link'
+        Rosh::Host::RemoteFileSystemObject.create('link', shell).
+          should eq 'the link'
+      end
+    end
+
+    context 'force_type is given' do
+      let(:fso) do
+        f = double 'Rosh::Host::RemoteFileSystemObject'
+        f.should_receive(:directory?).and_return false
+        f.should_receive(:file?).and_return false
+        f.should_receive(:link?).and_return false
+
+        f
+      end
+
+      it 'returns a new object of force_type' do
+        Rosh::Host::RemoteLink.should_receive(:new).with('link', shell).
+          and_return 'the link'
+        Rosh::Host::RemoteFileSystemObject.create('link', shell, force_type: :link).
+          should eq 'the link'
+      end
+    end
+  end
+
+  describe '#to_path' do
+    it 'returns the path that the object was created with' do
+      subject.to_path.should == path
+    end
+  end
+
+  describe '#file?' do
+    context 'command exit status is not 0' do
+      before do
+        shell.stub(:last_exit_status).and_return 1
+      end
+
+      it 'returns false' do
+        shell.should_receive(:exec).with '[ -f /file ]'
+
+        subject.file?.should be_false
+      end
+    end
+
+    context 'command exit status is 0' do
+      before do
+        shell.stub(:last_exit_status).and_return 0
+      end
+
+      it 'returns true' do
+        shell.should_receive(:exec).with '[ -f /file ]'
+
+        subject.file?.should be_true
+      end
+    end
+  end
+
+  describe '#directory?' do
+    context 'command exit status is not 0' do
+      before do
+        shell.stub(:last_exit_status).and_return 1
+      end
+
+      it 'returns false' do
+        shell.should_receive(:exec).with '[ -d /file ]'
+
+        subject.directory?.should be_false
+      end
+    end
+
+    context 'command exit status is 0' do
+      before do
+        shell.stub(:last_exit_status).and_return 0
+      end
+
+      it 'returns true' do
+        shell.should_receive(:exec).with '[ -d /file ]'
+
+        subject.directory?.should be_true
+      end
+    end
+  end
+
+  describe '#link?' do
+    context 'command exit status is not 0' do
+      before do
+        shell.stub(:last_exit_status).and_return 1
+      end
+
+      it 'returns false' do
+        shell.should_receive(:exec).with '[ -L /file ]'
+
+        subject.link?.should be_false
+      end
+    end
+
+    context 'command exit status is 0' do
+      before do
+        shell.stub(:last_exit_status).and_return 0
+      end
+
+      it 'returns true' do
+        shell.should_receive(:exec).with '[ -L /file ]'
+
+        subject.link?.should be_true
+      end
+    end
+  end
+
+  describe '#exists?' do
+    context 'command exit status is not 0' do
+      before do
+        shell.stub(:last_exit_status).and_return 1
+      end
+
+      it 'returns false' do
+        shell.should_receive(:exec).with '[ -e /file ]'
+
+        subject.exists?.should be_false
+      end
+    end
+
+    context 'command exit status is 0' do
+      before do
+        shell.stub(:last_exit_status).and_return 0
+      end
+
+      it 'returns true' do
+        shell.should_receive(:exec).with '[ -e /file ]'
+
+        subject.exists?.should be_true
+      end
+    end
+  end
+
+  describe '#owner' do
+    context 'command output is empty' do
+      it 'returns an empty string' do
+        shell.should_receive(:exec).with("ls -l /file | awk '{print $3}'").
+          and_return('')
+
+        subject.owner.should == ''
+      end
+    end
+
+    context 'command output contains the owner' do
+      it 'returns the owner' do
+        shell.should_receive(:exec).with("ls -l /file | awk '{print $3}'").
+          and_return("person\r\n")
+
+        subject.owner.should == 'person'
+      end
+    end
+  end
+
+  describe '#owner=' do
+    before do
+      subject.stub(:owner).and_return 'person'
+      shell.stub(:last_exit_status).and_return 0
+    end
+
+    context 'new_owner is the same as the old owner' do
+      it 'runs the command but does not update observers' do
+        shell.should_receive(:exec).with 'chown person /file'
+        subject.should_not_receive(:changed)
+        subject.should_not_receive(:notify_observers)
+
+        subject.owner = 'person'
+      end
+    end
+
+    context 'new_owner is not the same as the old owner' do
+      it 'runs the command and updates observers' do
+        shell.should_receive(:exec).with 'chown someone /file'
+        subject.should_receive(:changed)
+        subject.should_receive(:notify_observers).
+          with(subject, attribute: :owner, old: 'person', new: 'someone')
+
+        subject.owner = 'someone'
+      end
+    end
+
+    context 'command failed' do
+      before do
+        shell.stub(:last_exit_status).and_return 1
+      end
+
+      it 'runs the command but does not update observers' do
+        shell.should_receive(:exec).with 'chown someone /file'
+        subject.should_not_receive(:changed)
+        subject.should_not_receive(:notify_observers)
+
+        subject.owner = 'someone'
+      end
+    end
+  end
+
+  describe '#group' do
+    context 'command output is empty' do
+      it 'returns an empty string' do
+        shell.should_receive(:exec).with("ls -l /file | awk '{print $4}'").
+          and_return('')
+
+        subject.group.should == ''
+      end
+    end
+
+    context 'command output contains the group' do
+      it 'returns the group' do
+        shell.should_receive(:exec).with("ls -l /file | awk '{print $4}'").
+          and_return("people\r\n")
+
+        subject.group.should == 'people'
+      end
+    end
+  end
+
+  describe '#group=' do
+    before do
+      subject.stub(:group).and_return 'people'
+      shell.stub(:last_exit_status).and_return 0
+    end
+
+    context 'new_group is the same as the old group' do
+      it 'runs the command but does not update observers' do
+        shell.should_receive(:exec).with 'chgrp people /file'
+        subject.should_not_receive(:changed)
+        subject.should_not_receive(:notify_observers)
+
+        subject.group = 'people'
+      end
+    end
+
+    context 'new_group is not the same as the old group' do
+      it 'runs the command and updates observers' do
+        shell.should_receive(:exec).with 'chgrp strangers /file'
+        subject.should_receive(:changed)
+        subject.should_receive(:notify_observers).
+          with(subject, attribute: :group, old: 'people', new: 'strangers')
+
+        subject.group = 'strangers'
+      end
+    end
+
+    context 'command failed' do
+      before do
+        shell.stub(:last_exit_status).and_return 1
+      end
+
+      it 'runs the command but does not update observers' do
+        shell.should_receive(:exec).with 'chgrp strangers /file'
+        subject.should_not_receive(:changed)
+        subject.should_not_receive(:notify_observers)
+
+        subject.group = 'strangers'
+      end
+    end
+  end
+
+  describe '#mode' do
+    context 'command output is empty' do
+      it 'returns an empty string' do
+        shell.should_receive(:exec).with("ls -l /file | awk '{print $1}'").
+          and_return('')
+
+        subject.mode.should == nil
+      end
+    end
+
+    context 'command output contains the mode' do
+      it 'returns the mode' do
+        shell.should_receive(:exec).with("ls -l /file | awk '{print $1}'").
+          and_return("-rwxrwxrwx\r\n")
+
+        subject.mode.should == 777
+      end
+    end
+  end
+
+  describe '#mode=' do
+    before do
+      subject.stub(:mode).and_return 644
+      shell.stub(:last_exit_status).and_return 0
+    end
+
+    context 'new_mode is the same as the old mode' do
+      it 'runs the command but does not update observers' do
+        shell.should_receive(:exec).with 'chmod 644 /file'
+        subject.should_not_receive(:changed)
+        subject.should_not_receive(:notify_observers)
+
+        subject.mode = 644
+      end
+    end
+
+    context 'new_mode is not the same as the old mode' do
+      it 'runs the command and updates observers' do
+        shell.should_receive(:exec).with 'chmod 755 /file'
+        subject.should_receive(:changed)
+        subject.should_receive(:notify_observers).
+          with(subject, attribute: :mode, old: 644, new: 755)
+
+        subject.mode = 755
+      end
+    end
+
+    context 'command failed' do
+      before do
+        shell.stub(:last_exit_status).and_return 1
+      end
+
+      it 'runs the command but does not update observers' do
+        shell.should_receive(:exec).with 'chmod 111 /file'
+        subject.should_not_receive(:changed)
+        subject.should_not_receive(:notify_observers)
+
+        subject.mode = 111
+      end
+    end
+  end
+
+  describe '#basename' do
+    it 'returns the base name of the object' do
+      subject.basename.should eq 'file'
+    end
+  end
+
+  describe '#remove' do
+    before do
+      shell.stub(:last_exit_status).and_return 0
+      subject.stub(:exists?).and_return false
+    end
+
+    context 'object did not exist' do
+      it 'runs the command but does not update observers' do
+        shell.should_receive(:exec).with 'rm -rf /file'
+        subject.should_not_receive(:changed)
+        subject.should_not_receive(:notify_observers)
+
+        subject.remove
+      end
+    end
+
+    context 'object existed' do
+      before do
+        subject.stub(:exists?).and_return true
+      end
+
+      it 'runs the command and updates observers' do
+        shell.should_receive(:exec).with 'rm -rf /file'
+        subject.should_receive(:changed)
+        subject.should_receive(:notify_observers).
+          with(subject, attribute: :path, old: '/file', new: nil)
+
+        subject.remove
+      end
+    end
+
+    context 'command failed' do
+      before do
+        shell.stub(:last_exit_status).and_return 1
+      end
+
+      it 'runs the command but does not update observers' do
+        shell.should_receive(:exec).with 'rm -rf /file'
+        subject.should_not_receive(:changed)
+        subject.should_not_receive(:notify_observers)
+
+        subject.remove
+      end
+    end
+  end
+end
