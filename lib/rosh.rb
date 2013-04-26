6e53f9f137a9ad0a8ed8d08d62de61db2e48925a
Steve Loveless <steve.loveless@gmail.com>
2013-04-26 09:41:43 -0700

    Added docs

--- lib/rosh.rb
+++ lib/rosh.rb
@@ -7,6 +7,20 @@ class Rosh
   @hosts = {}
   @config = nil
 
+  # Adds a new Rosh::Host for Rosh to manage.
+  #
+  # @param [String] hostname Name or IP of the host to add.
+  # @param host_alias Any object to refer to the Host as.  Allows for shortcuts
+  #   to referring to the hostname.
+  #
+  # @example Add by hostname only
+  #   Rosh.add_host 'super-duper-server.example.com', user: 'robby', password: 'stuff'
+  #   Rosh.hosts['super-duper-server.example.com'].hostname   # => 'super-duper-server.example.com'
+  #
+  # @example Add by alias only
+  #   Rosh.add_host 'super-duper-server.example.com', host_alias: :super,
+  #     user: 'robby', password: 'stuff'
+  #   Rosh.hosts[:super'].hostname      # => 'super-duper-server.example.com'
   def self.add_host(hostname, host_alias: nil, **ssh_options)
     if host_alias.nil?
       @hosts[hostname] = Rosh::Host.new(hostname, ssh_options)
@@ -15,10 +29,16 @@ class Rosh
     end
   end
 
+  # The currently managed Rosh::Hosts.
+  #
+  # @return [Hash{String,Object => Rosh::Host}]
   def self.hosts
     @hosts
   end
 
+  # Reads the configuration from .roshrc.yml.
+  #
+  # @return [Hash]
   def self.config
     return @config if @config
 
@@ -28,6 +48,7 @@ class Rosh
     end
   end
 
+  # Resets hosts and configuration to empty values.
   def self.reset
     @hosts = {}
     @config = nil
