38b8233f9b06f84b87b9e0020a6bbfd629881b7b
Steve Loveless <steve.loveless@gmail.com>
2013-05-01 13:21:19 -0700

    Removed use of cache_is_dirty

--- spec/unit/rosh/host/package_managers/brew_spec.rb
+++ spec/unit/rosh/host/package_managers/brew_spec.rb
@@ -24,47 +24,32 @@ describe Rosh::Host::PackageManagers::Brew do
   end
 
   describe '#cache' do
-    context 'cache is dirty' do
-      let(:cache_dump) do
-        <<-DUMP
+    let(:cache_dump) do
+      <<-DUMP
 Formula                                         hub-1.10.4.tgz
 apple-gcc42-4.2.1-5666.3.pkg                    hub-1.10.5.tgz
 atk-2.6.0.tar.xz                                iftop-1.0pre2.tar.gz
 automake-1.12.2.tar.gz                          imagemagick-6.8.0-10.mountainlion.bottle.tar.gz
-        DUMP
-      end
-
-      before do
-        subject.instance_variable_set(:@cache, 'stuff')
-        subject.instance_variable_set(:@cache_is_dirty, true)
-
-        shell.should_receive(:exec).with('ls `brew --cache`').
-          and_return cache_dump
-      end
-
-      it 'returns an Hash of cached packages' do
-        cache = subject.cache
-
-        cache.should == {
-          'apple-gcc42' => { arch: '', version: '4.2.1-5666.3' },
-          'atk'         => { arch: '', version: '2.6.0' },
-          'automake'    => { arch: '', version: '1.12.2' },
-          'hub'         => { arch: '', version: '1.10.4' },
-          'hub'         => { arch: '', version: '1.10.5' },
-          'iftop'       => { arch: '', version: '1.0pre2' },
-          'imagemagick' => { arch: '', version: '6.8.0-10' },
-        }
-      end
+      DUMP
     end
 
-    context 'cache is not dirty' do
-      before do
-        subject.instance_variable_set(:@cache, cache)
-        subject.instance_variable_set(:@cache_is_dirty, false)
-      end
+    before do
+      shell.should_receive(:exec).with('ls `brew --cache`').
+        and_return cache_dump
+    end
 
-      let(:cache) { { 'package' => { arch: nil, version: nil } } }
-      specify { subject.cache.should eq cache }
+    it 'returns an Hash of cached packages' do
+      cache = subject.cache
+
+      cache.should == {
+        'apple-gcc42' => { arch: '', version: '4.2.1-5666.3' },
+        'atk'         => { arch: '', version: '2.6.0' },
+        'automake'    => { arch: '', version: '1.12.2' },
+        'hub'         => { arch: '', version: '1.10.4' },
+        'hub'         => { arch: '', version: '1.10.5' },
+        'iftop'       => { arch: '', version: '1.0pre2' },
+        'imagemagick' => { arch: '', version: '6.8.0-10' },
+      }
     end
   end
 
