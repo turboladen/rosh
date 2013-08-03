cdd6d488a86e89e21406e59c339e210c5a15eb26
Steve Loveless <steve.loveless@gmail.com>
2013-08-03 15:43:59 -0700

    Added todo list

---
+++ TODO.rb
@@ -0,0 +1,43 @@
+# TODO: CLI shell
+
+# TODO: check_state_first stuff for shells
+#   * local dir
+#   * local file
+#   * local link
+#   * remote dir
+#   * remote file
+#   * remote link
+#   * apt
+#   * dpkg
+#   * yum
+#   * deb
+#   * rpm
+#   * init
+#   * launch_ctl
+#   * group
+#   * user
+
+
+# TODO: Specs & Docs
+#   * local file
+#   * local link
+#   * remote dir
+#   * remote file
+#   * remote link
+#   * apt
+#   * dpkg
+#   * yum
+#   * deb
+#   * rpm
+#   * base
+#   * init
+#   * launch_ctl
+#   * attributes
+#   * file_system
+#   * group
+#   * group_manager
+#   * user
+#   * user_manager
+
+# TODO: turn off logging in specs
+# TODO: improve logging
