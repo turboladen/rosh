1a15a6e2418cdf018bf04d2ec953d847d41309c6
Steve Loveless <steve.loveless@gmail.com>
2013-08-04 14:51:13 -0700

    Added short class docs

--- lib/rosh/host/file_system_objects/local_link.rb
+++ lib/rosh/host/file_system_objects/local_link.rb
@@ -4,6 +4,8 @@ require_relative 'local_base'
 class Rosh
   class Host
     module FileSystemObjects
+
+      # Class for representing local links on disk.
       class LocalLink < LocalBase
 
       end
