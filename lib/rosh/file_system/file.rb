42d0c0c480bcaa26e70bd24ca77c61ea2cc39e54
Steve Loveless <steve.loveless@gmail.com>
2013-09-16 00:02:34 -0700

    Added some class docs

--- lib/rosh/file_system/file.rb
+++ lib/rosh/file_system/file.rb
@@ -6,6 +6,36 @@ require_relative 'api_stat'
 
 class Rosh
   class FileSystem
+
+    # A Rosh::FileSystem::File can represent either a file on a local host or a
+    # remote host.  It allows you to interact with a file on your file system
+    # in an object-oriented manner.
+    #
+    # Additionally, Files are observable: they can notify other objects when
+    # they change.  Observing objects must have an #update method defined that
+    # takes parameter list: `(changed_file, attribute: nil, old: nil, new: nil,
+    # as_sudo: nil)`.
+    #
+    #   class MyWatcher
+    #     def update(changed_file, attribute: nil, old: nil, new: nil, as_sudo: nil)
+    #       puts "File '#{changed_file.path}' changed:"
+    #       puts "  attribute:  #{attribute}"
+    #       puts "  old value:  #{old}"
+    #       puts "  new value:  #{new}"
+    #       puts "  using sudo? #{as_sudo}
+    #     end
+    #   end
+    #
+    #   watcher = MyWatcher.new
+    #   file = Rosh::FileSystem::File.new('/tmp/my_file', 'my_host.com')
+    #   file.add_observer(watcher)
+    #   file.mode           # => 755
+    #   file.chmod(644)
+    #   # =>  "File '/tmp/my_file' changed:"
+    #   #     "  attribute:  :mode"
+    #   #     "  old value:  755"
+    #   #     "  new value:  644"
+    #   #     "  using sudo? false"
     class File
       include Observable
       include APIBase
