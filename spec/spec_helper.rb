04d7de0d81df7afb10d241b2e672a09dfedc6b40
Steve Loveless <steve.loveless@gmail.com>
2013-10-26 16:44:51 -0700

    Added type metadata for functional vs unit specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,18 @@ SimpleCov.start do
 end
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
+Dir['spec/support/**/*.rb'].each { |f| require File.expand_path(f) }
+
+RSpec.configure do |config|
+  def config.escaped_path(*parts)
+    Regexp.compile(parts.join('[\\\/]') + '[\\\/]')
+  end
+
+  config.include Rosh::FunctionalExampleGroup, type: :functional, example_group: {
+    file_path: config.escaped_path(%w[spec functional])
+  }
+  config.include Rosh::UnitExampleGroup, type: :unit, example_group: {
+    file_path: config.escaped_path(%w[spec unit])
+  }
+end
 
